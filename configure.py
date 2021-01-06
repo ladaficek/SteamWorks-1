c7f3111699340986babbbd8948fad46591d3f4a4
NomisCZ <nomiscz@outlook.com>
2021-01-06 17:14:24 +0100

    Update AMDBuildScript, add new Github Actions workflow

--- configure.py
+++ configure.py
@@ -18,21 +18,22 @@ if ambuild_version.startswith('2.1'):
 	sys.stderr.write("AMBuild 2.2 or higher is required; please update\n")
 	sys.exit(1)
 
-run = run.BuildParser(sourcePath=sys.path[0], api='2.2')
-run.options.add_argument('--hl2sdk-root', type=str, dest='hl2sdk_root', default=None,
+parser = run.BuildParser(sourcePath=sys.path[0], api='2.2')
+parser.options.add_argument('--hl2sdk-root', type=str, dest='hl2sdk_root', default=None,
 		                   help='Root search folder for HL2SDKs')
-run.options.add_argument('--mms-path', type=str, dest='mms_path', default=None,
+parser.options.add_argument('--mms-path', type=str, dest='mms_path', default=None,
                        help='Path to Metamod:Source')
-run.options.add_argument('--sm-path', type=str, dest='sm_path', default=None,
+parser.options.add_argument('--sm-path', type=str, dest='sm_path', default=None,
                        help='Path to Sourcemod')
-run.options.add_argument('--steamworks-path', type=str, dest='steamworks_path', default=None,
+parser.options.add_argument('--steamworks-path', type=str, dest='steamworks_path', default=None,
                        help='Path to SteamWorks')
-run.options.add_argument('--enable-debug', action='store_const', const='1', dest='debug',
+parser.options.add_argument('--enable-debug', action='store_const', const='1', dest='debug',
                        help='Enable debugging symbols')
-run.options.add_argument('--enable-optimize', action='store_const', const='1', dest='opt',
+parser.options.add_argument('--enable-optimize', action='store_const', const='1', dest='opt',
                        help='Enable optimization')
-run.options.add_argument('-s', '--sdks', default='all', dest='sdks',
+parser.options.add_argument('-s', '--sdks', default='all', dest='sdks',
                        help='Build against specified SDKs; valid args are "all", "present", or '
                             'comma-delimited list of engine names')
-run.options.add_argument('--target', default=None, help='Override the default build target')
-run.Configure()
+parser.options.add_argument('--target', default=None, help='Override the default build target')
+
+parser.Configure()
