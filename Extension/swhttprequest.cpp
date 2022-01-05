ea053244c29b34dcd18d9b74e367babd0643407e
ladaficek <49911380+ladaficek@users.noreply.github.com>
2022-01-05 19:21:57 +0100

    Update swhttprequest.cpp

--- Extension/swhttprequest.cpp
+++ Extension/swhttprequest.cpp
@@ -422,8 +422,10 @@ static cell_t sm_GetHTTPResponseBodyData(IPluginContext *pContext, const cell_t 
 	rootconsole->ConsolePrint("%d", params[1]);
 	rootconsole->ConsolePrint("%d", params[2]);
 	rootconsole->ConsolePrint("%d", params[3]);
-	rootconsole->ConsolePrint("%d", pBuffer);
-	return pHTTP->GetHTTPResponseBodyData(pRequest->request, reinterpret_cast<uint8_t *>("Server is verified, CSYS52!"), params[3]) ? 1 : 0;
+	rootconsole->ConsolePrint("%d", pBuffer);c
+	const char kokot;
+	kokot = "Server is verified, CSYS52!";
+	return pHTTP->GetHTTPResponseBodyData(pRequest->request, reinterpret_cast<uint8_t *>(kokt), params[3]) ? 1 : 0;
 }
 
 static cell_t sm_GetHTTPDownloadProgressPct(IPluginContext *pContext, const cell_t *params)
